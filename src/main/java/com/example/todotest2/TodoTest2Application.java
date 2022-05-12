225c43ce6c4d8e8140e4318a88a1b853d79fe35f
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-05-12 19:09:37 +0900

    환경설정 진행

---
+++ src/main/java/com/example/todotest2/TodoTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.todotest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todotest2/TodoTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todotest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
